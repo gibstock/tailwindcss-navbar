cb15b5a7a87ded4f16772165a29c4f89313ed3e9
Andre Gonzales <gibstock@gmail.com>
2022-09-12 14:00:49 -0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: colors.trueGray,
+      }
+    },
   },
   plugins: [],
 }
